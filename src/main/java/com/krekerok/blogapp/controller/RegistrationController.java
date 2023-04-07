a115790b01426aee9a66e2c8125e8a7c321d376a
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-04-07 14:41:51 +0300

    Added swagger a documentation for register and verify methods

--- src/main/java/com/krekerok/blogapp/controller/RegistrationController.java
+++ src/main/java/com/krekerok/blogapp/controller/RegistrationController.java
@@ -2,8 +2,12 @@ package com.krekerok.blogapp.controller;
 
 import com.krekerok.blogapp.dto.requests.AppUserCreateDto;
 import com.krekerok.blogapp.service.RedisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +27,27 @@ public class RegistrationController {
     private RedisService redisService;
 
 
+    @Operation(summary = "User registration", description = "Create user and save in Redis database. The user's activation code is returned")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Create user",
+            content = @Content),
+        @ApiResponse(responseCode = "400", description = "Error: Username already exists or Error: Email already exists or any validation errors",
+            content = @Content)})
     @PostMapping
     public ResponseEntity<String> registerUser(@Valid @RequestBody AppUserCreateDto appUserCreateDto) {
         return new ResponseEntity<>(redisService.registerUser(appUserCreateDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Verify user email")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Successful activation"),
+        @ApiResponse(responseCode = "404", description = "Activation link is outdated",
+            content = @Content)})
     @PostMapping("/verify/{activationCode}")
-    public ResponseEntity<?> verifyUser(@PathVariable String activationCode, @RequestParam String email) {
+    public ResponseEntity<?> verifyUser(@PathVariable
+    @Parameter(description = "Activation code from the email link") String activationCode, @RequestParam String email) {
         redisService.verifyUser(email, activationCode);
         return new ResponseEntity<>(HttpStatus.OK);
     }
