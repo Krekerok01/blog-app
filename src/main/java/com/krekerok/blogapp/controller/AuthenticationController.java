3a80356a1684d6dcd14be9801a61c5937db73ba4
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-04-07 14:32:41 +0300

    Added swagger a documentation for the login method

--- src/main/java/com/krekerok/blogapp/controller/AuthenticationController.java
+++ src/main/java/com/krekerok/blogapp/controller/AuthenticationController.java
@@ -5,6 +5,11 @@ import static org.springframework.http.ResponseEntity.ok;
 import com.krekerok.blogapp.dto.requests.AppUserLoginDto;
 import com.krekerok.blogapp.dto.responses.AppUserLoginReadDto;
 import com.krekerok.blogapp.service.AppUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +25,21 @@ public class AuthenticationController {
     @Autowired
     private AppUserService appUserService;
 
+    @Operation(summary = "User login")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Login user",
+            content = {
+                @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = AppUserLoginReadDto.class))
+            }),
+        @ApiResponse(responseCode = "401", description = "If incorrect login or password.",
+            content = @Content)})
     @PostMapping("/login")
     public ResponseEntity<AppUserLoginReadDto> loginUser(@Valid @RequestBody AppUserLoginDto appUserLoginDto) {
-        AppUserLoginReadDto appUserAccountReadDto = appUserService.loginUser(appUserLoginDto);
-        return ok(appUserAccountReadDto);
+        AppUserLoginReadDto appUserLoginReadDto = appUserService.loginUser(appUserLoginDto);
+        return ok(appUserLoginReadDto);
     }
 }
