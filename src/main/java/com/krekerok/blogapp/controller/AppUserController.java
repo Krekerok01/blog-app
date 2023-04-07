abc4292093b8c0bfab7d0b0f678eb9bb63eb03f1
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-04-07 15:28:23 +0300

    Added swagger documentation to the AppUserController

--- src/main/java/com/krekerok/blogapp/controller/AppUserController.java
+++ src/main/java/com/krekerok/blogapp/controller/AppUserController.java
@@ -29,11 +29,23 @@ public class AppUserController {
     @Autowired
     private AppUserService appUserService;
 
+    @Operation(summary = "Get all users", description = "Get all users from database")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful request",
+            content = @Content),
+        @ApiResponse(responseCode = "404", description = "There are no users in the database",
+            content = @Content)})
     @GetMapping("/all")
     public ResponseEntity<List<AppUserReadDto>> getAllAppUsers(){
         return new ResponseEntity<>(appUserService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete user", description = "Delete user from database")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "No content if user was deleted from database",
+            content = @Content),
+        @ApiResponse(responseCode = "404", description = "Not found if user wasn't found in database",
+            content = @Content)})
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteAppUser(@PathVariable long id) {
         return appUserService.deleteAppUserById(id)
