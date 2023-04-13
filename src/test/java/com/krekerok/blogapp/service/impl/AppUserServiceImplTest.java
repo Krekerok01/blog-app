b2ac85c4a55230417333334053402e68a8816c55
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-04-13 17:42:49 +0300

    Added new test methods

--- src/test/java/com/krekerok/blogapp/service/impl/AppUserServiceImplTest.java
+++ src/test/java/com/krekerok/blogapp/service/impl/AppUserServiceImplTest.java
@@ -1,20 +1,31 @@
 package com.krekerok.blogapp.service.impl;
 
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.krekerok.blogapp.configuration.jwt.JwtUtils;
 import com.krekerok.blogapp.entity.AppUser;
+import com.krekerok.blogapp.entity.Blog;
 import com.krekerok.blogapp.entity.RedisUser;
 import com.krekerok.blogapp.entity.Role;
 import com.krekerok.blogapp.entity.RoleName;
 import com.krekerok.blogapp.repository.AppUserRepository;
 import com.krekerok.blogapp.service.RoleService;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
@@ -25,6 +36,8 @@ class AppUserServiceImplTest {
     private AppUserRepository appUserRepository;
     @Mock
     private RoleService roleService;
+    @Mock
+    private JwtUtils jwtUtils;
 
     @InjectMocks
     private AppUserServiceImpl appUserService;
@@ -32,16 +45,15 @@ class AppUserServiceImplTest {
 
 
     @Test
-    void createUser() {
-
+    void testCreateUser() {
         Role userRole = getUserRole();
         RedisUser redisUser = getRedisUser();
         AppUser appUser = getAppUser(userRole);
 
-        Mockito.doReturn(userRole).when(roleService).createRoleIfNotExist(RoleName.USER);
+        doReturn(userRole).when(roleService).createRoleIfNotExist(RoleName.USER);
         appUserService.createUser(redisUser);
 
-        Mockito.verify(appUserRepository).save(appUser);
+        verify(appUserRepository).save(appUser);
     }
 
     private AppUser getAppUser(Role userRole) {
@@ -70,6 +82,53 @@ class AppUserServiceImplTest {
             .timeOfSendingVerificationLink(Instant.now())
             .createdAt(Instant.now())
             .build();
+    }
+
+
+    @Test
+    void testDeleteAppUserById_WhenIdExists(){
+        long id = 1L;
+        AppUser appUser = new AppUser();
+        appUser.setUserId(id);
+
+        doReturn(Optional.of(appUser)).when(appUserRepository).findById(id);
+        doNothing().when(appUserRepository).delete(appUser);
+
+
+        boolean result = appUserService.deleteAppUserById(id);
+        assertTrue(result);
+        verify(appUserRepository, times(1)).findById(id);
+        verify(appUserRepository, times(1)).delete(appUser);
+        verify(appUserRepository, times(1)).flush();
+    }
+
+
+    @Test
+    void testDeleteAppUserById_WhenIdDoesNotExists(){
+
+        long id = 1L;
+
+        doReturn(Optional.empty()).when(appUserRepository).findById(id);
+
+        boolean result = appUserService.deleteAppUserById(id);
+
+        assertFalse(result);
+        verify(appUserRepository, times(1)).findById(id);
+        verify(appUserRepository, times(0)).delete(any(AppUser.class));
+        verify(appUserRepository, times(0)).flush();
+    }
+
+    @Test
+    void testDeleteLinkToTheBlog() {
+        Blog blog = mock(Blog.class);
+        AppUser appUser = mock(AppUser.class);
+
+        doReturn(appUser).when(appUserRepository).findAppUserByBlog(blog);
+
+        appUserService.deleteLinkToTheBlog(blog);
+
+        verify(appUser).setBlog(null);
+        verify(appUserRepository).save(appUser);
     }
 
 }
