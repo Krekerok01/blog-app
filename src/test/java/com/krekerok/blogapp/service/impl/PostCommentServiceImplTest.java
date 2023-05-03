0aea027af9cf04ab8f05260e04ed268416386126
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-05-03 21:12:20 +0300

    Removed unnecessary rows

--- src/test/java/com/krekerok/blogapp/service/impl/PostCommentServiceImplTest.java
+++ src/test/java/com/krekerok/blogapp/service/impl/PostCommentServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.krekerok.blogapp.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
@@ -14,10 +14,7 @@ import com.krekerok.blogapp.dto.responses.CommentResponseDto;
 import com.krekerok.blogapp.entity.AppUser;
 import com.krekerok.blogapp.entity.Post;
 import com.krekerok.blogapp.entity.PostComment;
-import com.krekerok.blogapp.entity.PostLike;
-import com.krekerok.blogapp.mapper.PostCommentMapper;
 import com.krekerok.blogapp.repository.PostCommentRepository;
-import com.krekerok.blogapp.repository.PostLikeRepository;
 import com.krekerok.blogapp.service.AppUserService;
 import com.krekerok.blogapp.service.PostService;
 import org.junit.jupiter.api.Test;
@@ -68,6 +65,4 @@ class PostCommentServiceImplTest {
             .message("test message")
             .build();
     }
-
-
 }
