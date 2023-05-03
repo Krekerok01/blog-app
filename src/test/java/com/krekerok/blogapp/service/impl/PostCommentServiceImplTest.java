8d8a010039d7fd6eaefb20838cf3472537b79d63
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-05-03 21:11:01 +0300

    Added a class to test PostCommentService

---
+++ src/test/java/com/krekerok/blogapp/service/impl/PostCommentServiceImplTest.java
@@ -0,0 +1,73 @@
+package com.krekerok.blogapp.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import com.krekerok.blogapp.dto.requests.CommentRequestDto;
+import com.krekerok.blogapp.dto.responses.CommentResponseDto;
+import com.krekerok.blogapp.entity.AppUser;
+import com.krekerok.blogapp.entity.Post;
+import com.krekerok.blogapp.entity.PostComment;
+import com.krekerok.blogapp.entity.PostLike;
+import com.krekerok.blogapp.mapper.PostCommentMapper;
+import com.krekerok.blogapp.repository.PostCommentRepository;
+import com.krekerok.blogapp.repository.PostLikeRepository;
+import com.krekerok.blogapp.service.AppUserService;
+import com.krekerok.blogapp.service.PostService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PostCommentServiceImplTest {
+
+    @Mock
+    private AppUserService appUserService;
+    @Mock
+    private PostService postService;
+    @Mock
+    private PostCommentRepository postCommentRepository;
+    @InjectMocks
+    private PostCommentServiceImpl postCommentService;
+
+    @Test
+    void testCreateComment() {
+
+        AppUser appUser = AppUser.builder().username("username").userId(1L).build();
+        Post post = Post.builder().postId(1L).build();
+        PostComment postComment = buildPostComment(appUser, post);
+
+        doReturn(appUser).when(appUserService).findAppUserByUsernameFromJWT(anyString());
+        doReturn(post).when(postService).findPostByPostId(anyLong());
+        doReturn(postComment).when(postCommentRepository).save(postComment);
+
+        CommentResponseDto result = postCommentService
+            .createComment(1L, new CommentRequestDto("test message"), anyString());
+
+        assertNotNull(result);
+        assertEquals(postComment.getPostId(), result.getPostId());
+        assertEquals(postComment.getAuthorUsername(), result.getAuthorUsername());
+        assertEquals(postComment.getMessage(), result.getMessage());
+        verify(postCommentRepository, times(1)).save(postComment);
+        verifyNoMoreInteractions(postCommentRepository);
+    }
+
+    private PostComment buildPostComment(AppUser appUser, Post post) {
+        return PostComment.builder()
+            .appUserId(appUser.getUserId())
+            .authorUsername(appUser.getUsername())
+            .postId(post.getPostId())
+            .message("test message")
+            .build();
+    }
+
+
+}
